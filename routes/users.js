e2c086c13fa49aaf80911d72a38f00c1c30d0ee9
Ivan Veselin <ivanveselin@hotmail.com>
2018-06-19 18:50:17 +0200

    Added volunteer register view and route

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,11 @@ router.get('/register', function (req, res) {
 	res.render('register');
 });
 
+//reister volunteer
+router.get('/register_volunteer', function (req, res) {
+	res.render('register_volunteer');
+});
+
 // Login
 router.get('/login', function (req, res) {
 	res.render('login');
@@ -79,6 +84,72 @@ router.post('/register', function (req, res) {
 		});
 	}
 });
+
+//register volunteer
+router.post('/register_volunteer', function (req, res) {
+	var name = req.body.name;
+	var email = req.body.email;
+	var oib = req.body.oib;
+	var address = req.body.address;
+	var city = req.body.city;
+	var phone = req.body.phone;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	// Validation
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('oib', 'Oib is required').notEmpty();
+	req.checkBody('address', 'Address is required').notEmpty();
+	req.checkBody('city', 'City is required').notEmpty();
+	req.checkBody('phone', 'Phone is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+	var errors = req.validationErrors();
+
+	if (errors) {
+		res.render('register', {
+			errors: errors
+		});
+	}
+	else {
+		//checking for email and oib are already taken
+		User.findOne({ oib: { 
+			"$regex": "^" + oib + "\\b", "$options": "i"
+	}}, function (err, user) {
+			User.findOne({ email: { 
+				"$regex": "^" + email + "\\b", "$options": "i"
+		}}, function (err, mail) {
+				if (user || mail) {
+					res.render('register', {
+						user: user,
+						mail: mail
+					});
+				}
+				else {
+					var newUser = new User({						
+						oib: oib,
+						password: password,
+						email: email,
+						name: name,
+						address: address,
+						city: city,
+						phone: phone
+					});
+					User.createUser(newUser, function (err, user) {
+						if (err) throw err;
+						console.log(user);
+					});
+         		req.flash('success_msg', 'You are registered and can now login');
+					res.redirect('/users/login');
+				}
+			});
+		});
+	}
+});
+
 
 passport.use(new LocalStrategy(
 	function (oib, password, done) {
